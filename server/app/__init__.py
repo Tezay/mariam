d92504de0795e2fce37f3ad740a7f901d3b873f0
Eliot <eliotcupillard06@gmail.com>
2026-07-06 17:42:20 +0200

    chore: release v0.13.0

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -251,7 +251,7 @@ def create_app(config_class=None):
         return {
             'status': 'healthy',
             'message': 'MARIAM API is running',
-            'version': '0.12.0',
+            'version': '0.13.0',
             'docs': '/docs'
         }
 
